d1848924c78e6989de5e00f73a95df114778408c
vinamcdev <vinamdev@proton.me>
2025-04-17 17:47:39 +0100

    build(client): Add client executable task

--- app/build.zig
+++ app/build.zig
@@ -4,12 +4,21 @@ pub fn build(b: *std.Build) void {
     const target = b.standardTargetOptions(.{});
     const optimize = b.standardOptimizeOption(.{});
 
-    const exe = b.addExecutable(.{
+    const server = b.addExecutable(.{
         .name = "server",
         .root_source_file = .{ .cwd_relative = "server/main.zig" },
         .target = target,
         .optimize = optimize,
     });
 
-    b.installArtifact(exe);
+    b.installArtifact(server);
+
+    const client = b.addExecutable(.{
+        .name = "client",
+        .root_source_file = .{ .cwd_relative = "client/main.zig" },
+        .target = target,
+        .optimize = optimize,
+    });
+
+    b.installArtifact(client);
 }
